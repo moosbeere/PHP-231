19373c48f5926b57e068b5e21598a57f2ed69b17
moosbeere <moosbeere_O@mail.ru>
2024-02-06 12:50:09 +0300

    Lab2 begin

---
+++ Lab2/index.php
@@ -0,0 +1,58 @@
+<?php
+    function separator(){
+        echo '<br>--------------<br>';
+    }
+
+    $m1 = array('summer');
+    print $m1[0].'<br>';
+
+    $m2[0] = 'winter';
+    var_dump($m2);
+    separator();
+
+    $m3 = array(3, 'automn', 100);
+    $m3[3] = 7;
+    $m3[5] = 'five';
+    $m3[6] = 22;
+    $m3['arr'] = 'arr';
+    var_dump($m3);
+    separator();
+
+    // for($i = 0; $i < count($m3); $i++){
+    //     echo $m3[$i].'<br>';
+    // }
+
+    separator();
+    foreach($m3 as $value){
+        echo $value.'<br>';
+    }
+    separator();    
+    foreach($m3 as $key=>$value){
+        echo $key.'=>'.$value.'<br>';
+    }
+
+    separator();
+    $a = array(1,2);
+    $b = array(1,'2');
+
+    $c = $a + $b;
+    var_dump($c);
+    echo '<br>';
+    foreach($c as $key => $value){
+        echo $key.'='.$value.'<br>';
+    }
+    separator();
+    $d = array(2 => 'a', 3 => 'b', 8 => 'c');
+    var_dump($a+$d);
+
+    separator();
+    var_dump($a == $b);//true
+    echo '<br>';
+    var_dump($a === $b);//false
+    echo '<br>';
+    var_dump($a != $b);//false
+    echo '<br>';
+    var_dump($a !== $b);//true
+    echo '<br>';
+
+    
