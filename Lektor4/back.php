501559948b3580370eb521c3db91bb5e321287f1
moosbeere <moosbeere_O@mail.ru>
2024-02-13 15:31:16 +0300

    Lektor4

---
+++ Lektor4/back.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        get_headers('https://getbootstrap.com/docs/5.3/forms/overview/#overview');
+    ?>
+    <a href="index.html">Переход на 1 стран</a>
+</body>
+</html>
