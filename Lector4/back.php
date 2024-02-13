96dbd835a4b9637141307a2a76047a9fe3707c72
moosbeere <moosbeere_O@mail.ru>
2024-02-13 13:32:17 +0300

    Lector4

---
+++ Lector4/back.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+        <?php
+            print_r(get_headers('https://getbootstrap.com/docs/5.3/forms/overview/#overview'));
+        ?>
+
+</body>
+</html>
